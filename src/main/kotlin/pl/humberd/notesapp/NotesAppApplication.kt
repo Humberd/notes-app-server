4922d2ddc9b3101954bcfbc9a954bad556696f73
Humberd <humberd2@gmail.com>
2019-07-21 19:00:42 +0200

    Save commit

--- src/main/kotlin/pl/humberd/notesapp/NotesAppApplication.kt
+++ src/main/kotlin/pl/humberd/notesapp/NotesAppApplication.kt
@@ -2,10 +2,21 @@ package pl.humberd.notesapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class NotesAppApplication
 
 fun main(args: Array<String>) {
     runApplication<NotesAppApplication>(*args)
+}
+
+
+@RestController
+class Foo {
+    @GetMapping
+    fun foo(): String {
+        return "pong"
+    }
 }
