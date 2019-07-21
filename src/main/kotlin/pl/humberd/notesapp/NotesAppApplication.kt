f19298807b592fc64cb07580b3a9641ac105a788
Humberd <humberd2@gmail.com>
2019-07-21 18:51:46 +0200

    Initial commit

---
+++ src/main/kotlin/pl/humberd/notesapp/NotesAppApplication.kt
@@ -0,0 +1,11 @@
+package pl.humberd.notesapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotesAppApplication>(*args)
+}
