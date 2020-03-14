57943fca19e1b3ba25f406de90838a321b17b55d
Humberd <humberd2@gmail.com>
2020-03-14 10:04:52 +0100

    Added initial sql migration

---
+++ src/main/resources/db/migration/V1__initial.sql
@@ -0,0 +1,33 @@
+drop table if exists User_Auth;
+drop table if exists User;
+
+create table User
+(
+    id         varchar2(30)  not null primary key,
+    name       varchar2(255) not null,
+    created_at timestamp     not null default current_timestamp,
+    updated_at timestamp     not null default current_timestamp on update current_timestamp
+);
+
+create table User_Auth
+(
+    user_id    varchar2(30) not null references User (id) on delete cascade,
+    created_at timestamp    not null default current_timestamp,
+    updated_at timestamp    not null default current_timestamp on update current_timestamp
+);
+
+insert into User(id, name)
+VALUES ('1', 'test');
+
+insert into User_Auth(user_id)
+values ('1');
+
+delete
+FROM User
+where id = '1'
+--
+-- insert into User
+--     (name)
+-- VALUES ('test')
+
+
