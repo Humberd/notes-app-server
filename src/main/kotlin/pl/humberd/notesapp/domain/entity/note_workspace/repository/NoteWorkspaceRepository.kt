2adac871bf656bc595f9163cde95d715c9f80307
Humberd <humberd2@gmail.com>
2020-04-24 22:48:33 +0200

    Fixed broken run

--- src/main/kotlin/pl/humberd/notesapp/domain/entity/note_workspace/repository/NoteWorkspaceRepository.kt
+++ src/main/kotlin/pl/humberd/notesapp/domain/entity/note_workspace/repository/NoteWorkspaceRepository.kt
@@ -2,7 +2,8 @@ package pl.humberd.notesapp.domain.entity.note_workspace.repository
 
 import org.springframework.stereotype.Repository
 import pl.humberd.notesapp.domain.configuration.repository.RefreshableJpaRepository
+import pl.humberd.notesapp.domain.entity.note_workspace.model.NoteWorkspace
 import pl.humberd.notesapp.domain.entity.note_workspace.model.NoteWorkspaceId
 
 @Repository
-interface NoteWorkspaceRepository : RefreshableJpaRepository<NoteWorkspaceRepository, NoteWorkspaceId>
+interface NoteWorkspaceRepository : RefreshableJpaRepository<NoteWorkspace, NoteWorkspaceId>
