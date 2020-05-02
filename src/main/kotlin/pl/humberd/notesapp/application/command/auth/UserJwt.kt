1adada253c7243e25a832b161c61ca034353f94a
Humberd <humberd2@gmail.com>
2020-05-02 19:13:10 +0200

    Removed unneccessary methods

--- src/main/kotlin/pl/humberd/notesapp/application/command/auth/UserJwt.kt
+++ src/main/kotlin/pl/humberd/notesapp/application/command/auth/UserJwt.kt
@@ -1,29 +1,10 @@
 package pl.humberd.notesapp.application.command.auth
 
-import pl.humberd.notesapp.application.exceptions.NotFoundException
 import pl.humberd.notesapp.domain.entity.user.model.UserId
 
 enum class AuthenticationProvider {
     PASSWORD_CREDENTIALS,
     GOOGLE;
-
-    companion object {
-        fun fromOathProvider(value: String): AuthenticationProvider {
-            return when (value) {
-                "google" -> GOOGLE
-                else -> throw NotFoundException(AuthenticationProvider::class, value)
-            }
-        }
-
-        fun isOauthProvider(value: String): Boolean {
-            return try {
-                fromOathProvider(value)
-                true
-            } catch (e: NotFoundException) {
-                false
-            }
-        }
-    }
 }
 
 
