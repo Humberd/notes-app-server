b4d2a720b895a967ed6731754b9f2f59abeb28bc
Humberd <humberd2@gmail.com>
2020-05-23 18:18:23 +0200

    Moved schema to initial file

--- src/main/resources/db/migration/R__initial.sql
+++ src/main/resources/db/migration/R__initial.sql
@@ -1,3 +1,16 @@
+drop table if exists user_group_membership_tag_trigger;
+drop table if exists user_group_membership;
+drop table if exists group_post_user_state;
+drop table if exists group_post;
+drop table if exists "group";
+drop table if exists resource_upvote;
+alter table if exists resource drop column latest_revision_id;
+drop table if exists resource_revision;
+drop table if exists resource;
+drop type if exists resource_type;
+drop type if exists resource_change_kind;
+
+
 drop table if exists Note_Workspace;
 drop table if exists Workspace;
 drop table if exists Note_Tag;
@@ -82,35 +95,6 @@ create trigger set_updated_at
     for each row
 execute procedure trigger_set_timestamp();
 
-------- Note
-create table Note
-(
-    id            varchar(32) not null primary key,
-    author_id     varchar(32) null references "user" (id) on delete set null,
-    url           text,
-    url_lc        text generated always as ( lower(url) ) stored,
-    title         text        not null,
-    content       text,
-    search_vector tsvector    not null,
-    created_at    timestamp   not null default now(),
-    updated_at    timestamp   not null default now()
-);
-
-create index search_vector_index on Note using gist (search_vector);
-create index url_lc_index on Note (url_lc);
-
-create trigger set_updated_at
-    before update
-    on Note
-    for each row
-execute procedure trigger_set_timestamp();
-
-create trigger set_search_vector
-    before insert or update
-    on Note
-    for each row
-execute procedure trigger_set_search_vector();
-
 -------- Tag
 create table Tag
 (
@@ -132,136 +116,72 @@ create trigger set_updated_at
 execute procedure trigger_set_timestamp();
 
 
--------- NoteTag
-create table Note_Tag
+---- resource-related tables
+create type resource_type as enum ('link', 'note');
+create type resource_change_kind as enum ('insert', 'update', 'delete');
+
+create table resource
 (
-    note_id    varchar(32) not null references Note (id) on delete cascade,
-    tag_id     varchar(32) not null references Tag (id) on delete cascade,
-    created_at timestamp   not null default now(),
-    updated_at timestamp   not null default now(),
-    PRIMARY KEY (note_id, tag_id)
+    id              varchar(32) not null primary key,
+    author_id       varchar(32) null references "user" (id) on delete set null,
+    revisions_count int         not null default 0,
+    created_at      timestamp   not null default now()
 );
 
-create trigger set_updated_at
-    before update
-    on Note_Tag
-    for each row
-execute procedure trigger_set_timestamp();
-
-create trigger increment_notes_count
-    after insert
-    on Note_Tag
-    for each row
-execute procedure trigger_increment_notes_count();
-
-create trigger decrement_notes_count
-    after delete
-    on Note_Tag
-    for each row
-execute procedure trigger_decrement_notes_count();
-
---------- Workspace
-create table Workspace
+create table resource_revision
 (
-    id         varchar(32)  not null primary key,
-    user_id    varchar(32)  not null references "user" (id) on delete cascade,
-    name       text         not null,
-    name_lc    varchar(255) not null unique generated always as ( lower(name) ) stored,
-    created_at timestamp    not null default now(),
-    updated_at timestamp    not null default now()
+    id          varchar(32)          not null primary key,
+    resource_id varchar(32)          not null references resource (id) on delete cascade,
+    change_kind resource_change_kind not null,
+    type        resource_type        not null,
+    payload     jsonb                not null,
+    created_at  timestamp            not null default now()
 );
 
-create trigger set_updated_at
-    before update
-    on Workspace
-    for each row
-execute procedure trigger_set_timestamp();
+alter table resource
+    add column latest_revision_id varchar(32) references resource_revision (id);
 
---------- NoteWorkspace
-create table Note_Workspace
+create table resource_upvote
 (
-    note_id      varchar(32) not null references Note (id) on delete cascade,
-    workspace_id varchar(32) not null references Workspace (id) on delete cascade,
-    created_at   timestamp   not null default now(),
-    updated_at   timestamp   not null default now(),
-    PRIMARY KEY (note_id, workspace_id)
+    resource_id varchar(32) not null references resource (id) on delete cascade,
+    user_id     varchar(32) not null references "user" (id) on delete cascade,
+    primary key (resource_id, user_id)
 );
 
-create trigger set_updated_at
-    before update
-    on Note_Workspace
-    for each row
-execute procedure trigger_set_timestamp();
+create table "group"
+(
+    id       varchar(32) not null primary key,
+    name     text        not null,
+    icon_url text        not null
+);
 
---
-insert into "user"(id, name, email)
-VALUES ('user-1', 'test', 'Admin@admin.com'),
-       ('user-2', 'foo', 'aa@aa.com');
+create table group_post
+(
+    id          varchar(32) not null primary key,
+    group_id    varchar(32) not null references "group" (id) on delete cascade,
+    resource_id varchar(32) not null references resource (id) on delete cascade,
+    unique (group_id, resource_id)
+);
 
---
-insert into Auth_Password_Credentials(user_id, password_hash)
-values ('user-1', '$2a$10$0T765q/oG9wvUDiYZ8EqGuIsA1wi4WrYWqRQ73Oj6tpeizyJdY0Pq');
+create table group_post_user_state
+(
+    group_post_id varchar(32) not null references group_post (id) on delete cascade,
+    user_id       varchar(32) not null references "user" (id) on delete cascade,
+    primary key (group_post_id, user_id)
+);
 
-insert into Note(id, author_id, url, title, content)
-VALUES ('note-1', 'user-1', null, 'k3s config export', '```kubectl config view --raw >~/.kube/config```'),
-       ('note-2', 'user-1', 'https://github.com/rancher/k3s/issues/703',
-        'k3s dns not resolving when docker is installed', '```
-sudo iptables -F
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-sudo reboot
-```'),
-       ('note-3', 'user-1', 'https://stackoverflow.com/a/54647323/4256929',
-        'inject() must be called from an injection context', 'seems like an issue when using npm link when consuming the library.
-Check out the following issue: https://github.com/angular/angular/issues/25813
-Spoiler: use "projects.$name.architect.build.options.preserveSymlinks: true" in angular.json'),
-       ('note-4', 'user-1', 'https://news.ycombinator.com/item?id=22749308', 'Hackernews jobs hiring', null),
-       ('note-5', 'user-1', 'https://findwork.dev/?source=hn', 'Findwork', null),
-       ('note-6', 'user-1', 'https://djqyo3vqv2.execute-api.us-west-1.amazonaws.com/latest/',
-        'Hackernews jobs hiring searcher', null),
-       ('note-7', 'user-1', 'https://hnjobs.emilburzo.com/', 'Hackernews jobs hiring search 2', null),
-       ('note-8', 'user-1', 'https://kennytilton.github.io/whoishiring/', 'Hackernews jobs hiring search 3', null),
-       ('note-9', 'user-1',
-        'https://blog.soshace.com/the-ultimate-guide-to-drag-and-drop-image-uploading-with-pure-javascript/',
-        'Drag and drop vanilla js', null);
+create table user_group_membership
+(
+    user_id  varchar(32) not null references "user" (id) on delete cascade,
+    group_id varchar(32) not null references "group" (id) on delete cascade,
+    primary key (user_id, group_id)
+);
 
-insert into Tag(id, user_id, name, background_color)
-values ('tag-1', 'user-1', 'kubernetes', '#ff00ff'),
-       ('tag-2', 'user-1', 'k3s', '#45de78'),
-       ('tag-3', 'user-1', 'docker', '#9ff37d'),
-       ('tag-bug', 'user-1', 'bug', '#ff0000'),
-       ('tag-angular', 'user-1', 'angular', null),
-       ('tag-build', 'user-1', 'build', null),
-       ('tag-job', 'user-1', 'Job', '#f032F1'),
-       ('tag-js', 'user-1', 'js', null);
-
-insert into Note_Tag(note_id, tag_id)
-values ('note-1', 'tag-1'),
-       ('note-1', 'tag-2'),
-       ('note-2', 'tag-1'),
-       ('note-2', 'tag-2'),
-       ('note-2', 'tag-3'),
-       ('note-3', 'tag-bug'),
-       ('note-3', 'tag-angular'),
-       ('note-3', 'tag-build'),
-       ('note-4', 'tag-job'),
-       ('note-5', 'tag-job'),
-       ('note-6', 'tag-job'),
-       ('note-7', 'tag-job'),
-       ('note-8', 'tag-job'),
-       ('note-9', 'tag-js');
-
-insert into Workspace(id, user_id, name)
-values ('workspace-work', 'user-1', 'Work'),
-       ('workspace-job-searching', 'user-1', 'Job Searching'),
-       ('workspace-private', 'user-1', 'Private'),
-       ('workspace-work-2', 'user-2', 'My Work');
-
-insert into Note_Workspace(note_id, workspace_id)
-values ('note-1', 'workspace-work'),
-       ('note-1', 'workspace-private'),
-       ('note-2', 'workspace-private'),
-       ('note-5', 'workspace-work'),
-       ('note-5', 'workspace-job-searching'),
-       ('note-5', 'workspace-private');
-
+create table user_group_membership_tag_trigger
+(
+    user_id  varchar(32) not null references "user" (id) on delete cascade,
+    group_id varchar(32) not null references "group" (id) on delete cascade,
+    tag_id   varchar(32) not null references tag (id) on delete cascade,
+    primary key (user_id, group_id, tag_id)
+);
 
